6d69f5fd3fe5679749fb6982fe4f7382730eeb66
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2021-12-31 12:15:32 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,20 @@
+﻿class Demo
+{
+    dynamic Data = 12;
+    public  int Method(int A, int B)
+    {
+        return (A + B) * Data;
+    }
+}
+class Program
+{
+    static  void Main(string[] args)
+    {
+        Demo Obj = new Demo();
+        dynamic value1 = 10;
+        dynamic value2 = 20;
+        dynamic Str = "Your Result Is:";
+        Console.WriteLine(Str + Obj.Method(value1, value2));
+        Console.ReadLine();
+    }
+}
