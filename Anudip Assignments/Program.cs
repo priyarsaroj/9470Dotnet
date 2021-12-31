a3ecdbb7752e1c8922e9df6823499ad4f51f12f9
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2021-12-31 12:12:18 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Practical3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i, j;
+            int[,] arr = new int[2, 2];
+            Console.WriteLine("Enter the elements for 2 X 2 matrix");
+            for (i = 0; i < 2; i++)
+            {
+                for (j = 0; j < 2; j++)
+                {
+                    Console.Write("Enter element at [{0},{1}] = ", i, j);
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (i = 0; i < 2; i++)
+            {
+                Console.Write("\n");
+                for (j = 0; j < 2; j++)
+                {
+                    Console.Write("{0}\t", arr[i, j]);
+                }
+            }
+        }
+    }
+}
