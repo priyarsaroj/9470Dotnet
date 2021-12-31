8a281538c5b8b86ed2685fa23ef9d06c51399100
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2021-12-31 12:24:25 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Test
+{
+    static void Main(string[] args)
+    {
+        string a = "Codes";
+        string b = "Dope";
+        int x = String.Compare(a, b);
+        int y = String.Compare(b, a);
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+    }
+}
