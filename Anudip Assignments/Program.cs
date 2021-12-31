fb050b019e73420cc01d5e2258d6e0a5fab0fcc4
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2021-12-31 12:10:41 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Practical2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] arr = new string[] { "C++", "Java", "C#" };
+            foreach (string a in arr)
+            {
+                Console.WriteLine(a);
+            }
+               Console.ReadLine();
+        }
+    }
+}
