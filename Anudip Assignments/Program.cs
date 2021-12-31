e462f8a8b4acf804c4c7cf3e4f4a2ff330bd63fa
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2021-12-31 12:17:39 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Demo
+{
+    public static void Main()
+    {
+        object[] obj = new object[2];
+        obj[0] = "pumpkin pie";
+        obj[1] = 18;
+        for (int i = 0; i < obj.Length; ++i)
+        {
+            string s = obj[i] as string;
+            Console.Write("{0}: ", i);
+            if (s != null)
+                Console.WriteLine("'" + s + "'");
+            else
+                Console.WriteLine("This is not a string!");
+        }
+        Console.ReadKey();
+    }
+}
