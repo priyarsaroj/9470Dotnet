446de8e2bcbb13cfb9b39f7829c2484553fa959b
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2021-12-31 12:23:39 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Test
+{
+    static void Main(string[] args)
+    {
+        string a = "Codes";
+        string b = "Dope";
+        String c = String.Concat(a, b);
+        Console.WriteLine(c);
+    }
+}
