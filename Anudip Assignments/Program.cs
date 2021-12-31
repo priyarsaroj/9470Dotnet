ab1bdbf8add88d57d9ac5a48bff419bb9e4907f1
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2021-12-31 12:34:44 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Decreasing
+{
+
+    // Main Method
+    public static void Main()
+    {
+
+        // declaring and initializing the array
+        int[] arr = new int[] { 1, 9, 6, 7, 5, 9 };
+
+        // Sort array in ascending order.
+        Array.Sort(arr);
+
+        // reverse array
+        Array.Reverse(arr);
+
+        // print all element of array
+        foreach (int value in arr)
+        {
+            Console.Write(value + " ");
+        }
+    }
+}
