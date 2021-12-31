c6b36e95232f508847c9859f30298c87e0c4d27e
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2021-12-31 12:19:23 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+namespace object_class_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Puran Singh Mehra";
+            int age = 36;
+            float marks = 89.50f;
+            Console.WriteLine(name);
+            Console.WriteLine(age.ToString());
+            Console.WriteLine(marks.ToString());
+            Console.ReadLine();
+        }
+    }
+}
