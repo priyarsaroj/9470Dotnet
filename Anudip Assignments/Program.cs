5d2371845a2d0814400744a4b7417af94cd37d4b
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2021-12-31 12:14:28 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Practical6
+{
+    class ImplicitTypedExample
+    {
+        public static void Main()
+        {
+            var a = 68;
+            var s = "candyfloss"; var arr = new[] { 1, 2, 3, 4};
+            Console.WriteLine(a);
+            Console.WriteLine(s);
+            Console.WriteLine(arr[3]);
+        }
+    }
+}
