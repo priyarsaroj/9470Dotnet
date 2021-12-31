4a350de72e36a4f0f4eae37e2e9ee39cfcebde64
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2021-12-31 12:30:23 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Test
+{
+    static void Main(string[] args)
+    {
+        string a = "CodesDope";
+        string b = a.Replace('o', 'd');
+        Console.WriteLine(b);
+    }
+}
