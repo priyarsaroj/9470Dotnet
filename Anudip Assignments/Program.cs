75d67ea300e67acdd063b6243f8447160428464b
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2021-12-31 12:31:58 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+struct str
+{
+    int a, b;
+    public str(int x,int y)
+    {
+        Console.WriteLine("Enter a No.");
+        a = Int32.Parse(Console.ReadLine());
+        b = Int32.Parse(Console.ReadLine());
+    }
+    public int sum()
+    {
+        return (a + b);
+    }
+}
+class student
+{
+    public static void Main()
+    {
+        str obj = new str();
+        int c = obj.show();
+        Console.WriteLine("Sum = "+c);
+    }
+}
