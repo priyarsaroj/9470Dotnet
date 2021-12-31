9c9250a1573c08f3549821a6db7093187222d14d
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2021-12-31 12:08:29 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Practical1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+         int[] arr = { 10, 11, 100, 101, 110 };
+            int len = arr.Length;
+            Console.WriteLine("Length of Array " + len);
+            Console.WriteLine(arr[0] + " " + arr[4]);
+        }
+    }
+}
