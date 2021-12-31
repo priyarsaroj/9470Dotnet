e3dcec7699ffe0dc117fe7ddd77f507f28cab0a5
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2021-12-31 12:20:37 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+namespace object_Class_Gethashcode
+{
+    class Program
+    {
+        class HashC
+        {
+            string name;
+            public HashC(string name)
+            {
+                this.name = name;
+            }
+            public override int GetHashCode()
+            {
+                string n = ToString();
+                return n.GetHashCode();
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            HashC ob1 = new HashC("Priya Rajmani Saroj");
+            Console.WriteLine(ob1.GetHashCode());
+            Console.ReadLine();
+        }
+    }
+}
