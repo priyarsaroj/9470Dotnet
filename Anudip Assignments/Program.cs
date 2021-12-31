5c320c1e1b6c2fad1154edca69496941bdbd93c1
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2021-12-31 12:32:43 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+struct student
+{
+    public String name, last;
+}
+struct Marks
+{
+    public student s;
+    public int math, sci;
+}
+class nest
+{
+    public static void Main()
+    {
+        Marks m;
+        m.s.name = "Priya";
+        m.s.last = "Saroj";
+        m.math = 75;
+        m.sci = 70;
+        Console.WriteLine("Name = " + m.s.name);
+        Console.WriteLine("Last Name = " + m.s.last);
+        Console.WriteLine("Math Marks = " + m.math);
+        Console.WriteLine("Science = " + m.sci);
+    }
+}
