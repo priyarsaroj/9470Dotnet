f14895d7a68e34b5395b62c4ec76357cb670f686
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2021-12-31 12:18:32 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CalculatorApplication
+{
+    class NumberManipulator
+    {
+        public void swap(ref int x, ref int y)
+        {
+            int temp;
+            temp = x; 
+            x = y; 
+            y = temp; 
+        }
+        static void Main(string[] args)
+        {
+            NumberManipulator n = new NumberManipulator();
+            int a = 600;
+            int b = 100;
+            Console.WriteLine("Before swap, value of a : {0}", a);
+            Console.WriteLine("Before swap, value of b : {0}", b);
+            n.swap(ref a, ref b);
+            Console.WriteLine("After swap, value of a : {0}", a);
+            Console.WriteLine("After swap, value of b : {0}", b);
+            Console.ReadLine();
+        }
+    }
+}
