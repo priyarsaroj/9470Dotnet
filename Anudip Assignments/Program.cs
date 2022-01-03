3c409308f5beba51f29381f5daeb3520407b38fa
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-01-03 11:23:32 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+using System.Text.RegularExpressions;
+
+namespace RegularExpression1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex r = new Regex(@"^\+?\d{0,2}\-?\d{4,5}\-?\d{5,6}");
+
+            string[] str = { "+91-9678967101", "9678967101", "+91-9678-967101", "+91-96789-67101", "+919678967101" };
+
+            foreach (string s in str)
+            {
+                Console.WriteLine("{0} {1} a valid mobile number.", s,
+                r.IsMatch(s) ? "is" : "is not");
+
+            }
+        }
+    }
+}
