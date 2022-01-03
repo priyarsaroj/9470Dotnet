32e281eaf8f040a77266adc17a4fcf5256c0a5f1
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-01-03 11:59:32 +0530

    Add files via upload

---
+++ Anudip Assignments/Program.cs
@@ -0,0 +1,20 @@
+﻿static void Main(string[] args)
+{
+    FileInfo file = null;
+    try
+    {
+        Console.Write("Enter a file name to write: ");
+        string fileName = Console.ReadLine();
+        file = new FileInfo(fileName);
+        file.AppendText("Hello World!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error occurred: {0}", ex.Message);
+    }
+    finally
+    {
+     
+        file = null;
+    }
+}
