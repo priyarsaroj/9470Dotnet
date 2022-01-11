c6ca9df90a37cfde9d079fe8fff0294d6562125c
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-01-11 12:11:24 +0530

    Add files via upload

---
+++ Anudip Practicals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace RegularExpression2
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hello World!");
+            string email = "priyasaroj202@gmail.com";
+            var result = Regex.IsMatch(email, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+            Console.WriteLine("is valid:{0}",result);
+            Console.ReadLine();
+        }
+    }
+}
