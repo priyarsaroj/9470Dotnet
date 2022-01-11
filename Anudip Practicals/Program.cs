f571a4f1e3ba07f7fbd0c5d30e5bc84a6377eb13
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-01-11 12:14:22 +0530

    Add files via upload

---
+++ Anudip Practicals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace RegularExpression2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hi,welcome@DotNet.com";
+
+            string result = Regex.Replace(str, "[^a-zA-Z0-9_]+", " ");
+            Console.WriteLine("is valid:{0}", result);
+            Console.ReadLine();
+        }
+    }
+}
