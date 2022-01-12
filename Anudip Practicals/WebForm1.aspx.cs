129760a0883b8fd0892ded001029fbd71d5cdef8
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-01-12 15:21:38 +0530

    Add files via upload

---
+++ Anudip Practicals/WebForm1.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace program1
+
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            ArrayList al = new ArrayList();
+            string str = "Priya Saroj";
+            int x = 7;
+            DateTime d = DateTime.Parse("8-oct-1985");
+            al.Add(str);
+            al.Add(x);
+            al.Add(d);
+
+            foreach (object o in al)
+            {
+                Response.Write(o);
+                Response.Write("<br>");
+            }
+        }
+    }
+}
