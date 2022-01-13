6a6cb9330614c341adfa23bbbc55cbebd24cf4f0
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-01-13 12:30:30 +0530

    Add files via upload

---
+++ Anudip Practicals/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace D
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Queue q = new Queue();
+            q.Enqueue("cs.net");
+            q.Enqueue("vb.net");
+            q.Enqueue("asp.net");
+            q.Enqueue("sqlserver");
+            
+            foreach (object o in q)
+            {
+                Response.Write(o + "<br>");
+            }
+        }
+    }
+}
