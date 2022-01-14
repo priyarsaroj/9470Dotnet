a9d9f163cc4c7536facfc853bd8256bfd4b97621
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-01-14 12:05:02 +0530

    Add files via upload

---
+++ Anudip Practicals/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace delegate_Example4
+{
+    class Program
+    {
+        public delegate void delmethod(int x, int y);
+        public class TestMultipleDelegate
+        {
+            public void plus_Method1(int x, int y)
+            {
+                Console.Write("You are in plus_Method");
+                Console.WriteLine(x + y);
+
+}
+            public void subtract_Method2(int x, int y)
+            {
+                Console.Write("You are in subtract_Method");
+                Console.WriteLine(x - y);
+            }
+        }
+        static void Main(string[] args)
+        {
+            TestMultipleDelegate obj = new TestMultipleDelegate();
+            delmethod del = new delmethod(obj.plus_Method1);
+           
+            del += new delmethod(obj.subtract_Method2);
+            
+            del(50, 10);
+            Console.WriteLine();
+           
+            del -= new delmethod(obj.plus_Method1);
+           
+            del(20, 10);
+            Console.ReadLine();
+        }
+    }
+}
