3ceb53f6e0f5cd336d6a0b8105a80f1e8d6e3237
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-01-14 12:49:32 +0530

    Add files via upload

---
+++ Anudip Practicals/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace delegate_custom
+{
+    class Program
+    {
+        public delegate void MyDelegate(int a);
+        public class XX
+        {
+            event MyDelegate MyEvent;
+            public void RaiseEvent()
+            {
+                MyEvent(20);
+                Console.WriteLine("Event Raised");
+            }
+            public void Display(int x)
+            {
+                Console.WriteLine("Display Method {0}", x);
+            }
+        }
+        void Main(string[] args)
+        {
+            XX obj = new XX();
+            obj.MyEvent += new MyDelegate(obj.Display);
+            obj.RaiseEvent();
+            Console.ReadLine();
+        }
+    }
+}
