da30ea4eaab692aa550b53d0693d02799b973530
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-01-17 11:23:06 +0530

    Add files via upload

---
+++ Anudip Practicals/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		// Store the path of the textfile in your system
+		string file = @"C:\Anudip Priya";
+
+		Console.WriteLine("Reading File using File.ReadAllText()");
+
+		// To read the entire file at once
+		if (File.Exists(file))
+		{
+			// Read all the content in one string
+			// and display the string
+			string str = File.ReadAllText(file);
+			Console.WriteLine(str);
+		}
+		Console.WriteLine();
+
+		Console.WriteLine("Reading File using File.ReadAllLines()");
+
+		// To read a text file line by line
+		if (File.Exists(file))
+		{
+			// Store each line in array of strings
+			string[] lines = File.ReadAllLines(file);
+
+			foreach (string ln in lines)
+				Console.WriteLine(ln);
+		}
+		Console.WriteLine();
+
+		Console.WriteLine("Reading File using StreamReader");
+
+		// By using StreamReader
+		if (File.Exists(file))
+		{
+			// Reads file line by line
+			StreamReader Textfile = new StreamReader(file);
+			string line;
+
+			while ((line = Textfile.ReadLine()) != null)
+			{
+				Console.WriteLine(line);
+			}
+
+			Textfile.Close();
+
+			Console.ReadKey();
+		}
+		Console.WriteLine();
+	}
+}
