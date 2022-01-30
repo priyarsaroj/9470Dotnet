77134b8872e5d76930c6f32563fe757ccc75d7b9
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-01-30 13:16:55 +0530

    Add files via upload

---
+++ Anudip Practicals/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace data_reader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+            string sql = null;
+            SqlDataReader dataReader;
+            connetionString = "Data Source=.;Initial Catalog=priya;Integrated Security=True";
+            sql = "Your SQL Statement Here , like Select * from product";
+            connection = new SqlConnection(connetionString);
+            try
+            {
+                connection.Open();
+                command = new SqlCommand(sql, connection);
+                dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    MessageBox.Show(dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + " - " + dataReader.GetValue(2));
+                }
+                dataReader.Close();
+                command.Dispose();
+                connection.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+        }
+    }
+}
+        
+    
+
