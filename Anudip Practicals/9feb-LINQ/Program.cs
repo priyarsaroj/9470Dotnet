2197d34f77cab08dcc9188187bdfa95d9f31e0ca
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-02-09 12:24:34 +0530

    Add files via upload

---
+++ Anudip Practicals/9feb-LINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    public static void Main()
+    {
+        int[] nums = { 6, -2, 30, -3, 0, -8, 12, 7, 8, 9, 100 };
+        // Query for obtains positive values less than 10.
+        var result = from n in nums
+                     where n > 0
+                     where n < 10
+                     select n;
+        Console.Write("The positive values less than 10: ");
+        // Execute the query.
+        foreach (int i in result)
+        {
+            Console.Write(i + " ");
+        }
+        Console.ReadLine();
+    }
+}
