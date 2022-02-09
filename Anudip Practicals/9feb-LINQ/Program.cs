71d4ced431942346a88b81d3cf23115493d392cd
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-02-09 12:19:59 +0530

    Add files via upload

---
+++ Anudip Practicals/9feb-LINQ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+class aggregate
+{
+    public static void Main()
+    {
+        int[] nums = { 10, 20, 30, 40, 50, 1, 2, 3, 4, 5 };
+        Console.WriteLine("Total count = " + nums.Count());
+        Console.WriteLine("Average     = " + nums.Average());
+        Console.WriteLine("Summation   = " + nums.Sum());
+        Console.WriteLine("Max value   = " + nums.Max());
+        Console.WriteLine("Min Value   = " + nums.Min());
+        Console.ReadLine();
+    }
+}
