c52ef53de5a048aac5abbe0cc8614f3ea0a6d704
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-02-09 12:18:55 +0530

    Add files via upload

---
+++ Anudip Practicals/9feb-LINQ/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Linq
+{
+    class distinct
+    {
+        static void Main(string[] args)
+        {
+            string[] countries = { "UK", "India", "Australia", "uk", "india", "USA" };
+            IEnumerable<string> result = countries.Distinct(StringComparer.OrdinalIgnoreCase); 
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+}
