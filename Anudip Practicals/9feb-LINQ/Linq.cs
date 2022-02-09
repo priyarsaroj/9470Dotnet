7ab649b6527041ff85de974a18167ea8eb5c8c7a
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-02-09 12:24:03 +0530

    Rename Program.cs to Linq.cs

---
+++ Anudip Practicals/9feb-LINQ/Linq.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    public static void Main()
+    {
+        int[] nums = { 10, -6, 3, 0, -4, 25 };
+        // Query that selects only positive numbers.
+        var result = from n in nums
+                     where n > 0
+                     select n;
+        Console.Write("The positive values in nums: ");
+        // Execute the query.
+        foreach (int i in result)
+        {
+            Console.Write(i + " ");
+        }
+        Console.ReadLine();
+    }
+}
