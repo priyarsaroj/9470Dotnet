27c6cd6671ad49581608a331b8b8152c42d41509
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-02-09 14:09:49 +0530

    Add files via upload

---
+++ Anudip Assignments/9feb-LinqWithobject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqWithObject
+{
+    class LinqToStringArray
+    {
+        static void Main(string[] args)
+        {
+            //create an array of type string  
+            string[] array = { "Vaishali", "Shalu", "Akshay", "Akki" };
+            /*IEnumerable will iterate over the collection of data use Linq query to select the particular element which starts from s*/
+            IEnumerable<string> result = from a in array where a.ToLowerInvariant().StartsWith("s") select a;
+            //foreach loop is used to print the output which is in the result  
+            foreach (string item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
