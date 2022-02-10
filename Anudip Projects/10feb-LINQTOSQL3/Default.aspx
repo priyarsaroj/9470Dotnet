8d5e6b80d0dd520f137dd11f81f598c5a2a05801
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-02-10 13:53:53 +0530

    Add files via upload

---
+++ Anudip Projects/10feb-LINQTOSQL3/Default.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="LINQTOSQL3.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+<title>LINQ to SQL Insert, Update, Delete Operations in ASP.Net Gridview</title>
+<style type="text/css">
+.GridviewDiv {font-size: 100%; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Arial, Helevetica, sans-serif; color: #303933;}
+.headerstyle
+{
+color:#FFFFFF;border-right-color:#abb079;border-bottom-color:#abb079;background-color: #df5015;padding:0.5em 0.5em 0.5em 0.5em;text-align:center;
+}
+</style>
+</head>
+<body>
+<form id="form1" runat="server">
+<div class="GridviewDiv">
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" ShowFooter="True" AllowPaging="True" DataKeyNames="Id,Name" OnPageIndexChanging="GridView1_PageIndexChanging" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" OnRowDeleting="GridView1_RowDeleting" OnRowCommand ="GridView1_RowCommand"> 
+<HeaderStyle CssClass="headerstyle" />
+<Columns>
+<asp:BoundField DataField="Id" HeaderText="Id" ReadOnly="true" />
+<asp:TemplateField HeaderText="Name">
+<ItemTemplate>
+<asp:Label ID="lblName" runat="server" Text='<%# Eval("Name")%>'/>
+</ItemTemplate>
+<EditItemTemplate>
+<asp:TextBox ID="txtName" runat="server" Text='<%# Eval("Name")%>'/>
+</EditItemTemplate>
+<FooterTemplate>
+<asp:TextBox ID="txtfName" runat="server" />
+</FooterTemplate>
+</asp:TemplateField>
+<asp:TemplateField HeaderText = "Location">
+<ItemTemplate>
+<asp:Label ID="lblLocation" runat="server" Text='<%# Eval("Location")%>'></asp:Label>
+</ItemTemplate>
+<EditItemTemplate>
+<asp:TextBox ID="txtLocation" runat="server" Text='<%# Eval("Location")%>'/>
+</EditItemTemplate>
+<FooterTemplate>
+<asp:TextBox ID="txtfLocation" runat="server" />
+</FooterTemplate>
+</asp:TemplateField>
+<asp:TemplateField HeaderText="Gender">
+<ItemTemplate>
+<asp:Label ID="lblGender" runat="server" Text='<%# Eval("Gender")%>'/>
+</ItemTemplate>
+<EditItemTemplate>
+<asp:TextBox ID="txtGender" runat="server" Text='<%# Eval("Gender")%>'/>
+</EditItemTemplate>
+<FooterTemplate>
+<asp:TextBox ID="txtfGender" runat="server" />
+<asp:Button ID="btnAdd" CommandName="AddNew" runat="server" Text="Add" />
+</FooterTemplate>
+</asp:TemplateField>
+<asp:CommandField ShowEditButton="True" ShowDeleteButton="true" />
+</Columns>
+</asp:GridView>
+<asp:Label ID="lblresult" runat="server"></asp:Label>
+</div>
+    </form>
+    </body>
+    </html>
---
+++ Anudip Projects/10feb-LINQTOSQL3/Default.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+
+namespace LINQTOSQL3
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        EmployeeDBDataContext db = new EmployeeDBDataContext();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                BindGridview();
+            }
+        }
+        protected void BindGridview()
+        {
+            var result = from ed in db.EmployeeDetails
+                         select new
+                         {
+                             Id = ed.EmpId,
+                             Name = ed.EmpName,
+                             Location = ed.Location,
+                             Gender = ed.Gender
+                         };
+            GridView1.DataSource = result;
+            GridView1.DataBind();
+        }
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName.Equals("AddNew"))
+            {
+                TextBox txtname = (TextBox)GridView1.FooterRow.FindControl("txtfName");
+                TextBox txtlocation = (TextBox)GridView1.FooterRow.FindControl("txtfLocation");
+                TextBox txtgender = (TextBox)GridView1.FooterRow.FindControl("txtfGender");
+                EmployeeDetail emp = new EmployeeDetail();
+                emp.EmpName = txtname.Text;
+                emp.Location = txtlocation.Text;
+                emp.Gender = txtgender.Text;
+                db.EmployeeDetails.InsertOnSubmit(emp);
+                db.SubmitChanges();
+                lblresult.ForeColor = Color.Green;
+                lblresult.Text = txtname.Text + " details inserted successfully";
+                BindGridview();
+            }
+        }
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            BindGridview();
+        }
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            BindGridview();
+        }
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            BindGridview();
+        }
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int empid = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values["Id"].ToString());
+            TextBox txtname = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtName");
+            TextBox txtlocation = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtLocation");
+            TextBox txtgender = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtGender");
+            EmployeeDetail emp = new EmployeeDetail();
+            emp = db.EmployeeDetails.Single(x => x.EmpId == empid);
+            emp.EmpName = txtname.Text;
+            emp.Location = txtlocation.Text;
+            emp.Gender = txtgender.Text;
+            db.SubmitChanges();
+            GridView1.EditIndex = -1;
+            BindGridview();
+            lblresult.ForeColor = Color.Green;
+            lblresult.Text = txtname.Text + " details updated successfully";
+        }
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int empid = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values["Id"].ToString());
+            string empname = GridView1.DataKeys[e.RowIndex].Values["Name"].ToString();
+            EmployeeDetail emp = new EmployeeDetail();
+            emp = db.EmployeeDetails.Single(x => x.EmpId == empid);
+            db.EmployeeDetails.DeleteOnSubmit(emp);
+            db.SubmitChanges();
+            BindGridview();
+            lblresult.ForeColor = Color.Green;
+            lblresult.Text = empname + " details deleted successfully";
+        }
+    }
+}
