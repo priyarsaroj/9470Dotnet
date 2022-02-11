1bfe172a7271e70e3ff971b5c2ef5b3de5b08b51
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-02-11 14:36:42 +0530

    Add files via upload

---
+++ Anudip Practicals/11feb-LINQTOENTITIES/LINQTODATASET/SQLQuery2.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[EmployeeDetails1](
+[EmpId] INT IDENTITY (1, 1) NOT NULL,
+[EmpName] VARCHAR (50) NULL,
+[Location] VARCHAR (50) NULL,
+[Gender] VARCHAR (20) NULL
+PRIMARY KEY CLUSTERED ([EmpId] ASC)
+);
+
+insert into EmployeeDetails1 values ('Suresh Dasari','Chennai','Male')
+insert into EmployeeDetails1 values ('Rohini Alavala','Chennai','Female')
+insert into EmployeeDetails1 values ('Praveen Alavala','Guntur','Male')
+insert into EmployeeDetails1 values ('Sateesh Chandra','Vizag','Male')
+insert into EmployeeDetails1 values ('Sushmitha','Vizag','Female')
