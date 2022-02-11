e4fb9123e757d23a6403e727a2f344955a57001e
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-02-11 14:17:55 +0530

    Add files via upload

---
+++ Anudip Practicals/11feb-LINQTOENTITIES/LINQTOENTITY/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINQTOENTITY
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!Page.IsPostBack)
+            {
+                BindGridData();
+            }
+        }
+        private void BindGridData()
+        {
+            // Create the object of the Context defined in the constructor of the context class.
+            priyaEntities1 db = new priyaEntities1();
+            var result = from emp in db.EmployeeDetails1
+                         select new
+                         {
+                             Id = emp.EmpId,
+                             Name = emp.EmpName,
+                             Location = emp.Location,
+                             Gender = emp.Gender
+                         };
+            GridView1.DataSource = result.ToList();
+            GridView1.DataBind();
+        }
+    }
+}
