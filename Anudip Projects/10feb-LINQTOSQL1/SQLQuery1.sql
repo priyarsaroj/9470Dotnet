b6ae3a3f1c49dc4b648bd77ac3aadebad75f5c8e
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-02-13 17:08:11 +0530

    Add files via upload

---
+++ Anudip Projects/10feb-LINQTOSQL1/SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿Create table StudentData  
+(  
+    RollNumber int primary key,  
+    StudentName varchar(50),  
+    FatherName varchar(50),  
+    MotherName varchar(50)    
+)  
+
+Insert into StudentData(RollNumber,StudentName,FatherName,MotherName) values(101,'Akshay','Rakesh Tyagi','Samlesh Tyagi')  
+Insert into StudentData(RollNumber,StudentName,FatherName,MotherName) values(102,'Vaishali','Ashok Tyagi','Munesh Tyagi')  
+Insert into StudentData(RollNumber,StudentName,FatherName,MotherName) values(103,'Arpita','Arvind Rai','Anni Rai')  
+
+Select *from StudentData;
+
