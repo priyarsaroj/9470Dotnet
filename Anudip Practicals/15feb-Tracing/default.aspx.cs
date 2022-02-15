312730018a98d51033ca3eb88168931d522a5229
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-02-15 14:30:47 +0530

    Add files via upload

---
+++ Anudip Practicals/15feb-Tracing/default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tracing2
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //bind the grid view  
+            //connection string  
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-50BB7SC;Initial Catalog=priya;Integrated Security=True");
+            //write query  
+            string query = "select * from employee2";
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+            DataSet ds = new DataSet();
+            //fill in dataset  
+            da.Fill(ds);
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+        }
+    }
+}
+
+        
