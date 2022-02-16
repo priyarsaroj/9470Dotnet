88c781a62057210c01199ce1c7bafdf93f9ee35c
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-02-17 05:16:26 +0530

    Add files via upload

---
+++ Anudip Practicals/16feb-WCF/WcfTst/SQLQuery2.sql
@@ -0,0 +1,9 @@
+create table employee(
+FirstName varchar(40),
+LastName varchar(40),
+Email varchar(40)
+);
+
+insert into employee values('Priya','Saroj','priyasaroj@gmail.com');
+
+select * from employee;
