ac36afd995cf6145fa42aea895df3f5ff78aebe5
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-02-22 11:31:38 +0530

    Add files via upload

---
+++ Anudip Practicals/21feb-PayMentRESTService/IPayMentService.cs
@@ -0,0 +1,31 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace PayMentRESTService
+{
+
+    [ServiceContract]
+    public interface IPayMentService
+    {
+        //To Insert or POST Records
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/AddPayee/{Name}/{City}", BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        void AddPayee(string Name, string City);
+
+        //To Get Records from database 
+        [OperationContract]
+        [WebInvoke(Method = "GET",UriTemplate = "/PayBill/{PayId}", BodyStyle = WebMessageBodyStyle.Wrapped,RequestFormat = WebMessageFormat.Json,ResponseFormat = WebMessageFormat.Json)]
+        string PayBill(string PayId);
+
+        //To Update records
+        [OperationContract]
+        [WebInvoke(Method = "PUT", UriTemplate = "/UpdateBillPayment/{PayId}/{TransId}", BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        void UpdateBillPayment(string PayId,string TransId);
+
+        //To delete records
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", UriTemplate = "/RemovePayee/{Id}", BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        void RemovePayee(string Id);
+
+    }
+}
---
+++ Anudip Practicals/21feb-PayMentRESTService/PayMentService.svc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PayMentRESTService
+{
+
+    public class PayMentService : IPayMentService
+    {
+        public void AddPayee(string Name, string City)
+        {
+           //write database related insert logic here.
+        }
+        public string PayBill(string PayId)
+        {
+            return "Transaction having PayId " + PayId + " is successful";
+            //write database related data retrival logic here.
+        }
+
+        public void RemovePayee(string Id)
+        {
+            //write database related delete logic here.
+        }
+
+        public void UpdateBillPayment(string PayId, string TransId)
+        {
+            //write database related update logic here.
+        }
+    }
+}
