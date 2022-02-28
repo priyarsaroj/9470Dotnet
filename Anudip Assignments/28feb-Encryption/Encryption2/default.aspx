6b976b8c7e0970552f1165e26f6088b947dfd28f
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-02-28 13:37:32 +0530

    Add files via upload

---
+++ Anudip Assignments/28feb-Encryption/Encryption2/default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="Encryption2._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        body
+        {
+            font-family: Arial;
+            font-size: 10pt;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+<u>Encrypt</u><br />
+<br />
+Original Text:
+<asp:TextBox ID="txtOriginalText" runat="server" Text="" />
+<br />
+<br />
+Encrypted Text:
+<asp:Label ID="lblEncryptedText" runat="server" Text="" />
+<br />
+<br />
+<asp:Button ID="btnEncrypt" OnClick="Encrypt" Text="Encrypt" runat="server" />
+<hr />
+<u>Decrypt</u>
+<br />
+<br />
+Encrypted Text:
+<asp:TextBox ID="txtEncryptedText" runat="server" Text="" />
+<br />
+<br />
+Decrypted Text:
+<asp:Label ID="lblDecryptedText" runat="server" Text="" />
+<br />
+<br />
+<asp:Button ID="btnDecrypt" OnClick="Decrypt" Text="Decrypt" runat="server" />
+    </form>
+</body>
+</html>
+
---
+++ Anudip Assignments/28feb-Encryption/Encryption2/default.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Encryption2
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Encrypt(object sender, EventArgs e)
+        {
+            lblEncryptedText.Text = this.Encrypt(txtOriginalText.Text.Trim());
+        }
+
+        private string Encrypt(string clearText)
+        {
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+
+        protected void Decrypt(object sender, EventArgs e)
+        {
+            lblDecryptedText.Text = this.Decrypt(txtEncryptedText.Text.Trim());
+        }
+
+        private string Decrypt(string cipherText)
+        {
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+    }
+}
+
