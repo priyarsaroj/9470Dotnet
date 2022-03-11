2c307883e04e1be99fce9ebf41156b87089f21ec
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-03-11 17:06:24 +0530

    Add files via upload

---
+++ Anudip Projects/EmployeeCRUD/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeCRUD.Models
+{
+    public class Employee
+    {
+            [Key]
+            public int Id { get; set; }
+            [Required]
+            [Display(Name = "Employee Name")]
+            public string Name { get; set; }
+            public string Designation { get; set; }
+            [DataType(DataType.MultilineText)]
+            public string Address { get; set; }
+            public DateTime? RecordCreatedOn { get; set; }
+
+        }
+    }
---
+++ Anudip Projects/EmployeeCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
