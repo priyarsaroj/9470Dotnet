6d2d25a6730f922c8ed6633efcf191b603c9fb83
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-01 13:04:20 +0530

    Add files via upload

---
+++ Lab Exercise/ArrayList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_List
+{
+    class Program
+    {
+
+        // Main Method
+        public static void Main(string[] args)
+        {
+
+            // Create a list of strings
+            ArrayList al = new ArrayList();
+            al.Add("Priyanka");
+            al.Add("Nimesh");
+            al.Add(1);
+            al.Add(6.5);
+
+            // Iterate list element using foreach loop
+            foreach (var names in al)
+            {
+                Console.WriteLine(names);
+            }
+        }
+    }
+
+}
