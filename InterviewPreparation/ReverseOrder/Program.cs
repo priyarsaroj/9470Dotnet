5417d1d4caa01e436d06e68f2981585c483a2e59
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-01 13:33:22 +0530

    Add files via upload

---
+++ InterviewPreparation/ReverseOrder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] list = { 10, 100, 1000, 10000 };
+            int[] temp = list;
+            Console.Write("Original Array: ");
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            // reverse the array
+            Array.Reverse(temp);
+            Console.Write("Reversed Array: ");
+            foreach (int i in temp)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
