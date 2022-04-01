490943d8a91fcb9f5140c783b80a39070f3b278c
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-01 13:04:00 +0530

    Add files via upload

---
+++ Lab Exercise/Array/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+
+        // Main Method
+        public static void Main(string[] args)
+        {
+
+            // creating array
+            int[] arr = new int[4];
+
+            // initializing array
+            arr[0] = 47;
+            arr[1] = 77;
+            arr[2] = 87;
+            arr[3] = 97;
+
+            // traversing array
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+
+}
