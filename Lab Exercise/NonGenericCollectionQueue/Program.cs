35413a043b7c1f7f61171ded6b926b642b99aa2c
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-01 13:05:17 +0530

    Add files via upload

---
+++ Lab Exercise/NonGenericCollectionQueue/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGenericCollectionQueue
+{
+    class Program
+    {
+
+        // Driver code
+        public static void Main()
+        {
+
+            // Creating a Queue
+            Queue myQueue = new Queue();
+
+            // Inserting the elements into the Queue
+            myQueue.Enqueue("C#");
+            myQueue.Enqueue("PHP");
+            myQueue.Enqueue("Perl");
+            myQueue.Enqueue("Java");
+            myQueue.Enqueue("C");
+
+            // Displaying the count of elements
+            // contained in the Queue
+            Console.Write("Total number of elements present in the Queue are: ");
+
+            Console.WriteLine(myQueue.Count);
+
+            // Displaying the beginning element of Queue
+            Console.WriteLine("Beginning Item is: " + myQueue.Peek());
+        }
+    }
+
+}
