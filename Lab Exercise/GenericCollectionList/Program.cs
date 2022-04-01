190364dfbdf02cf69ec4f5af38d39c91d884d85e
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-01 13:04:57 +0530

    Add files via upload

---
+++ Lab Exercise/GenericCollectionList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollectionList
+{
+    class Program
+    {
+
+        // Main Method
+        public static void Main(String[] args)
+        {
+
+            // Creating a List of integers
+            List<int> mylist = new List<int>();
+
+            // adding items in mylist
+            for (int j = 5; j < 10; j++)
+            {
+                mylist.Add(j * 3);
+            }
+
+            // Displaying items of mylist
+            // by using foreach loop
+            foreach (int items in mylist)
+            {
+                Console.WriteLine(items);
+            }
+        }
+    }
+
+}
