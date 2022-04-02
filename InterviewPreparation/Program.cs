a5a529f8460a8d8dfc1f316f2b0ddd8f01cda7ec
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-02 14:56:01 +0530

    Add files via upload

---
+++ InterviewPreparation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Enter a number:\t");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2==0) 
+            {
+                Console.WriteLine("{0} is even number", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is odd number", num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
