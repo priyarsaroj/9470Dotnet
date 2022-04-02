dfafd3cb331e8ac0d66cd4df1867ec929161db1e
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-02 15:59:22 +0530

    Add files via upload

---
+++ InterviewPreparation/ArraySort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySort
+{
+    internal class Program
+    {
+        static void printarray(int[] arr)
+        {
+            Console.WriteLine("\nElements of array:\n");
+            foreach (int i in arr)
+            {
+                Console.WriteLine("\t{0}", i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int [5];
+            int i;
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write("Enter number:\t");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Program.printarray(arr);
+            Array.Sort(arr);
+            Program.printarray(arr);
+            Console.ReadLine();
+        }
+    }
+}
