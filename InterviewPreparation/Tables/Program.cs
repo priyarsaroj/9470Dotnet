d885bec700781c792131107bc19cfd617e0f29c5
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-02 16:08:20 +0530

    Add files via upload

---
+++ InterviewPreparation/Tables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, num;
+            Console.WriteLine("Enter number to print table: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= 10; i++)
+            {        
+                Console.Write("{0} X {1} = {2} \n", num, i, num * i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
