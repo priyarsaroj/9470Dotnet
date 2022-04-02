ae07994b88f12527535645cd7dc7732fadc22325
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-02 15:22:36 +0530

    Add files via upload

---
+++ InterviewPreparation/FindDay/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            label:
+            Console.WriteLine("\nEnter a number(1-7)\t");
+            a=Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+               case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option\n");
+                    goto label;
+            }
+            Console.ReadLine();
+        }
+    }
+}
