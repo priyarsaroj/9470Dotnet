d3d7c7ee5b29f41e28db8442399f94a5c110177b
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-02 15:35:49 +0530

    Add files via upload

---
+++ InterviewPreparation/LoopExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0, j=0;
+            for (i = 0; i <= 5; i++) 
+            {
+                for (j = 0; j < i; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
