c82ced2730bbcebac49e3b2001851903e84aab75
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-04 11:31:36 +0530

    Add files via upload

---
+++ InterviewPreparation/CheckedUncheckedException/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckedUncheckedException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte num1 = 20, num2 = 30, result;
+            try
+            {
+                unchecked
+                {
+                    result = (sbyte)(num1 * num2);
+                    Console.WriteLine("{0}x{1} = {2}", num1, num2, result);
+                }
+                checked
+                {
+                    result = (sbyte)(num1 * num2);
+                    Console.WriteLine("{0}x{1} = {2}", num1, num2, result);
+                }
+            }
+            catch (OverflowException oe)
+            { 
+                Console.WriteLine(oe.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
