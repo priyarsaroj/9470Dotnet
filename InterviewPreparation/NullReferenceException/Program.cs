2a92883364ea207ab25beef689ee7d29662a4965
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-04 11:43:18 +0530

    Add files via upload

---
+++ InterviewPreparation/NullReferenceException/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullReference_Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = null;
+            try
+            {
+                int length = value.Length;
+                Console.WriteLine(length);
+                Console.ReadLine();
+            }
+            catch (NullReferenceException ne)
+            {
+                Console.WriteLine(ne.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
