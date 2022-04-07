6ec118882a94cf47ff2e9d24ffad143fcbc54d6c
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-07 15:54:24 +0530

    Add files via upload

---
+++ Lab Exercise/TwoDimensionalArray/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Write a program to create two multidimensional arrays of same size.
+   Accept value from user and store them in first array. 
+   Now copy all the elements of first array are second array and print output.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiDimensionalArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr1 = new int[3, 3];
+            int[,] arr2 = new int[3, 3];
+            int i, j;
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    Console.Write("Enter array value:\t");
+                    arr1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    arr2[i, j] = arr1[i, j];
+                }
+            }
+
+            Console.WriteLine("\n\nElements of second array are:\n\n");
+            for (i = 0; i < 3; i++)
+            {
+                Console.WriteLine();
+                for (j = 0; j < 3; j++)
+                {
+                    Console.Write("\t{0}", arr2[i, j]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
