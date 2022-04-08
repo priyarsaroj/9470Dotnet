b96a4937cd5f66a8af4e158c14965e0367cdf4b4
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-08 11:03:21 +0530

    Add files via upload

---
+++ InterviewPreparation/Generic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    //declare generics
+    public class GenericClass<T>
+    {
+        public void Genericfunction(T printvalue)
+        {
+            Console.WriteLine(printvalue);
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Printing integer value:");
+            GenericClass<int> i= new GenericClass<int>();
+            i.Genericfunction(100);
+            Console.WriteLine("Printing string value:");
+            GenericClass<string> s= new GenericClass<string>();
+            s.Genericfunction("Priya");
+
+        }
+    }
+    
+}
