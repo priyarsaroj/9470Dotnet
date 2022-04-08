88b68dacea9c9f1cd5519fcc9dc5f830816cb293
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-08 11:48:22 +0530

    Add files via upload

---
+++ InterviewPreparation/ForLoop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+            Console.Write("Input number of rows : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= n - i; j++)
+                    Console.Write(" ");
+                for (j = 1; j <= i; j++)
+                    Console.Write("{0}", j);
+                //for (j = i - 1; j >= 1; j--)
+//Console.Write("{0}", j);
+                Console.Write("\n");
+            }
+        }
+    }
+}
