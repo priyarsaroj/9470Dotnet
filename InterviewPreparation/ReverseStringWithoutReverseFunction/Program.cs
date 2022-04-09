de2f4cd86fe58bcda732a4d0f1d138deee0103c6
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-09 13:56:44 +0530

    Add files via upload

---
+++ InterviewPreparation/ReverseStringWithoutReverseFunction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseStringWithoutReverseFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Str, Revstr = ""; 
+            int Length; 
+            Console.Write("Enter a string : "); 
+            Str = Console.ReadLine(); 
+            Length = Str.Length - 1; 
+            while (Length >= 0)  
+            {
+                Revstr = Revstr + Str[Length];  
+                Length--;
+            }
+            Console.WriteLine("Reverse  string  is:  {0}", Revstr); 
+            Console.ReadLine();
+        }
+    }
+}
