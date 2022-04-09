14229dd65d471b50ec951a127c4ccc1b388e1666
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-09 13:55:49 +0530

    Add files via upload

---
+++ InterviewPreparation/Substrings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a String : ");
+            string inputString = Console.ReadLine();
+
+            Console.WriteLine("All substrings for given string are : ");
+
+            for (int i = 0; i < inputString.Length; ++i)
+            {
+                StringBuilder subString = new StringBuilder(inputString.Length - i);
+                for (int j = i; j < inputString.Length; ++j)
+                {
+                    subString.Append(inputString[j]);
+                    Console.Write(subString + " ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+    
+
