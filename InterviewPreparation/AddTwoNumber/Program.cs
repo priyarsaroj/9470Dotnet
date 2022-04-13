b9f9451c024025048bf72b4a1580b963aeb9db0e
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-13 11:24:16 +0530

    Add files via upload

---
+++ InterviewPreparation/AddTwoNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter the first number:");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            b=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The sum of two numbers is: {0}", (a+b));
+        }
+    }
+}
