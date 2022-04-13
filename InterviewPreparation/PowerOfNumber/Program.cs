ed55230bdce2cbd1c323819a517d026de0b79142
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-13 11:25:28 +0530

    Add files via upload

---
+++ InterviewPreparation/PowerOfNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOfNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a,b;
+            Console.WriteLine("Enter a number:");
+            a =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give the Exponent :");
+            b = Convert.ToInt32(Console.ReadLine());
+            double value1 = Math.Pow(a, b);
+            Console.WriteLine("Result : {0}", value1);
+            Console.ReadLine();
+        }
+    }
+}
