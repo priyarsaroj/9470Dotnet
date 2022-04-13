4cc008e5a7e836893222661cd6604b05ea60b515
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-13 10:33:50 +0530

    Add files via upload

---
+++ Anudip Projects/InventoryManager/Models/Helper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventoryBeginners.Models;
+using CodesByAniz.Tools;
+
+namespace InventoryBeginners.Models
+{
+    public static class Helper
+    {
+        
+        public static string GetTypeName(string fullTypeName)
+        {
+            string retString = "";
+
+            try
+            {
+                int lastIndex = fullTypeName.LastIndexOf('.') + 1;
+                retString = fullTypeName.Substring(lastIndex, fullTypeName.Length - lastIndex);
+            }
+            catch 
+            {
+                retString = fullTypeName;
+            }
+            return retString.Replace("]","");
+        }
+    }
+}
---
+++ Anudip Projects/InventoryManager/Models/Product.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventoryBeginners.Models
+{
+    public class Product
+    {
+
+        [Key]
+        [StringLength(6)]
+        public string Code { get; set; }
+
+        [Required]
+        [StringLength(75)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "smallmoney")]
+        public decimal Cost { get; set; }
+
+        [Required]
+        [Column(TypeName = "smallmoney")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [ForeignKey("Units")]
+        [Display(Name="Unit")]
+        public int UnitId { get; set; }
+        public virtual Unit Units { get; set; }
+
+      
+        [ForeignKey("Brands")]
+        [Display(Name = "Brand")]
+        public int? BrandId { get; set; }
+        public virtual Brand Brands { get; set; }
+
+        [ForeignKey("Categories")]
+        [Display(Name = "Category")]
+        public int? CategoryId { get; set; }
+        public virtual Category Categories { get; set; }
+
+        [ForeignKey("ProductGroups")]
+        [Display(Name = "ProductGroup")]
+        public int? ProductGroupId { get; set; }
+        public virtual ProductGroup ProductGroups { get; set; }
+
+        [ForeignKey("ProductProfiles")]
+        [Display(Name = "ProductProfile")]
+        public int? ProductProfileId { get; set; }
+        public virtual ProductProfile ProductProfiles { get; set; }
+
+        public string PhotoUrl { get; set; } = "noimage.png";
+
+        [Display(Name = "Product Photo")]
+        [NotMapped]
+        public IFormFile ProductPhoto { get; set; }
+
+        [NotMapped]
+        public string BreifPhotoName { get; set; }
+
+    }
+}
