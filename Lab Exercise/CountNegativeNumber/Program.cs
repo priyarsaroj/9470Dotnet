20a4b446017a9383a2f2c81b9920506ce25131ba
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-14 10:32:43 +0530

    Add files via upload

---
+++ Lab Exercise/CountNegativeNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountNegativeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr = new int[5];
+            int i,num;
+            Console.WriteLine("Enter size of the array: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter elements in array: ");
+            for (i = 0; i < num; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("All negative elements in array are: ");
+            for (i = 0; i < num; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
