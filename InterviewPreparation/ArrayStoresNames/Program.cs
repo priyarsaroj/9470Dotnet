b8c0862660aff41e0da70146718987f49eac8ab7
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-15 14:44:58 +0530

    Add files via upload

---
+++ InterviewPreparation/ArrayStoresNames/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Create a C# program that requests three names of people from the user and stores them in an array of objects of type Person. 
+   To do this, first create a Person class that has a Name property of type string and override the ToString() method.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayStoresNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 3;
+            Person[] persons = new Person[total];
+            Console.WriteLine("Enter three names:");
+            for (int i = 0; i < total; i++)
+            {
+                persons[i] = new Person()
+                {
+                    Name = Console.ReadLine()
+                };
+            }
+
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine(persons[i].ToString());
+            }
+
+            Console.ReadLine();
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return "Hello! My name is " + Name;
+            }
+        }
+    }
+}
