8f17008989e76331cad7bc85da5644eb89460f7a
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-15 14:59:48 +0530

    Add files via upload

---
+++ InterviewPreparation/Inheritance/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 3;
+            Person[] persons = new Person[total];
+            Console.WriteLine("Enter three names:");
+            for (int i = 0; i < total; i++)
+            {
+                if (i == 0)
+                {
+                    persons[i] = new Teacher(Console.ReadLine());
+                }
+                else
+                {
+                    persons[i] = new Student(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < total; i++)
+            {
+                if (i == 0)
+                {
+                    ((Teacher)persons[i]).Explain();
+
+                }
+                else
+                {
+                    ((Student)persons[i]).Study();
+                }
+            }
+        }
+
+        public class Person
+        {
+            protected string Name { get; set; }
+
+            public Person(string name)
+            {
+                Name = name;
+            }
+
+            public override string ToString()
+            {
+                return "Hello! My name is " + Name;
+            }
+
+            ~Person()
+            {
+                Name = string.Empty;
+            }
+        }
+
+        public class Teacher : Person
+        {
+            public Teacher(string name) : base(name)
+            {
+                Name = name;
+            }
+
+            public void Explain()
+            {
+                Console.WriteLine("Explain");
+            }
+        }
+
+        public class Student : Person
+        {
+            public Student(string name) : base(name)
+            {
+                Name = name;
+            }
+
+            public void Study()
+            {
+                Console.WriteLine("Study");
+            }
+        }
+    }
+}
+    
+
