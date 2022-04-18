6d3e8e621cbf4e46b1c385b64c5b9f500b7faf4f
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-18 15:14:57 +0530

    Add files via upload

---
+++ InterviewPreparation/ConversionDollarsToRupees/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionDollarsToRupees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter dollars:");
+            double dollars = Convert.ToDouble(Console.ReadLine());
+            double rupees = dollars * 64;
+            Console.WriteLine(rupees + " Rupees");
+        }
+    }
+}
