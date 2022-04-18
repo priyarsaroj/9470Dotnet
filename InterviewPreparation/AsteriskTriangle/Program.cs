12e8921d9578e076d94f98dfd0df58943c6c0c38
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-18 13:25:37 +0530

    Add files via upload

---
+++ InterviewPreparation/AsteriskTriangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsteriskTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows;
+            Console.Write("Input number of rows : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+        }
+    }
+}
