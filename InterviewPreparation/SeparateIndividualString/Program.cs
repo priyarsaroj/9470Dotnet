d8ccf58903d9a059fe2191f2833cd6d8d2964d94
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-18 13:22:36 +0530

    Add files via upload

---
+++ InterviewPreparation/SeparateIndividualString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeparateIndividualString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int l = 0;
+            Console.Write("Input the string : ");
+            str = Console.ReadLine();
+            Console.Write("The characters of the string are  :  ");
+            while (l <= str.Length - 1)
+            {
+                Console.Write("{0} ", str[l]);
+                l++;
+            }
+            Console.Write("\n\n");
+        }
+    }
+}
