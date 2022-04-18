2b9949de599369b015c35e4226c955153ddd8999
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-18 15:14:30 +0530

    Add files via upload

---
+++ InterviewPreparation/PowerOfNumberWithoutMathFunc/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOfNumberWithoutMathFunc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseNumber, expNumber, result = 1;
+            Console.Write("Enter a Base Number : ");
+            baseNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a exponent Number : ");
+            expNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            while (expNumber != 0)
+            {
+                result *= baseNumber;
+                --expNumber;
+            }
+
+            Console.WriteLine("Result = {0}", result);
+
+            Console.ReadLine();
+        }
+    }
+}
