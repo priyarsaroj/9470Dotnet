bff271ce9f4a9944c9a1679f11402d01fc95f63e
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-18 13:26:30 +0530

    Add files via upload

---
+++ InterviewPreparation/LessThan100GreaterThan200/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessThan100GreaterThan200
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a first number(<100): ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input a second number(>200): ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((m < 100 && n > 200));
+        }
+    }
+}
