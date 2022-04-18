f2f67723cee967d48bf56e221c95300f7ca0633f
priyarsaroj <94603411+priyarsaroj@users.noreply.github.com>
2022-04-18 13:26:01 +0530

    Add files via upload

---
+++ InterviewPreparation/PyramidNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyramidNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, a, rows, k, t = 1;
+            Console.Write("input number of rows : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            a = rows + 4 - 1;  // spc space
+            for (i = 1; i <= rows; i++)
+            {
+                for (k = a; k >= 1; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= i; j++)
+                    Console.Write("{0} ", t++);
+                Console.Write("\n");
+                a--;
+            }
+        }
+    }
+}
